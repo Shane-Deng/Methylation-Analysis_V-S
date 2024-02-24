4ee500a9ed3536311bd5084d0fd06bc89284a4ca
Shanekk <152445703+Shane-Deng@users.noreply.github.com>
2024-02-24 12:57:05 -0500

    Add files via upload

---
+++ dependcy installation.py
@@ -0,0 +1,29 @@
+import subprocess
+
+env_name = str(input("What do you want to call your environment").strip())
+# Create and activate a new Conda environment (Modify 'myenv' and Python version as needed)
+python_version = str(input("Which python version do you want? 3.8 is recommended").strip())
+subprocess.run(f"conda create --name {env_name} python={python_version} -y", shell=True)
+subprocess.run(f"conda activate {env_name}", shell=True)
+
+# Install Python packages with conda install
+python_packages = [
+    "pyfiglet",
+    "termcolor",
+    "sendgrid"
+]
+for package in python_packages:
+    subprocess.run(f"conda install -c conda-forge {package} -y", shell=True)
+
+# Install external bioinformatics tools with conda install
+bioinformatics_tools = [
+    "bismark",
+    "bowtie2",
+    "samtools"
+]
+for tool in bioinformatics_tools:
+    subprocess.run(f"conda install -c bioconda {tool} -y", shell=True)
+
+print(f"Installation completed. Please activate the new environment with: conda activate {env_name}")
+
+
