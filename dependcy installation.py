0f670cd4af5617690e945ad88ae0e4fd3b5244ec
Shanekk <152445703+Shane-Deng@users.noreply.github.com>
2024-02-24 13:22:23 -0500

    Update dependcy installation.py

--- dependcy installation.py
+++ dependcy installation.py
@@ -3,7 +3,6 @@ import subprocess
 env_name = str(input("What do you want to call your environment").strip())
 # Create and activate a new Conda environment (Modify 'myenv' and Python version as needed)
 python_version = str(input("Which python version do you want? 3.8 is recommended").strip())
-subprocess.run(f"conda init bash")
 subprocess.run(f"conda create --name {env_name} python={python_version} -y", shell=True)
 subprocess.run(f"conda activate {env_name}", shell=True)
 
@@ -25,6 +24,6 @@ bioinformatics_tools = [
 for tool in bioinformatics_tools:
     subprocess.run(f"conda install -c bioconda {tool} -y", shell=True)
 
-print(f"Installation completed. Please do the following step:\n 1. run "conda init bash" \n2. run"conda activate {env_name}")
+print(f"Installation completed. Please use conda activate {env_name} to activate you conda environment")
 
 
